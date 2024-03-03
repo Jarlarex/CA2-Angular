8ed34cae1fdeb6e7d55ed3392dce6f6d628e44ce
Jarlarex <jarla1335@gmail.com>
2024-03-03 17:06:58 +0000

    Movie Finder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OmdbApiService } from './services/omdb-api.service';
+import { NgIf, NgFor, CommonModule } from '@angular/common';
+import { OMDBResponse } from './omdbresponse';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NgFor, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'CA2';
+  movieData?: OMDBResponse;
+  errorMessage: string = '';
+
+  constructor(private _omdbService: OmdbApiService) { }
+
+  getMovieDetails(movieName: string): void {
+    this._omdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData = movieData;
+        console.log("Director name : " + this.movieData.Director);
+      },
+      error => this.errorMessage = error
+    );
+  }
 }
---
+++ src/app/omdbresponse.ts
@@ -0,0 +1,7 @@
+export interface OMDBResponse {
+    Title: string;
+    Year: string;
+    Director: string;
+    Poster: string;
+    Error: string;
+}
